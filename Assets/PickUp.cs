9d63948a39c8fea779b1026f46b196d5c2b3187d
Elliot <93895160+2Elliot@users.noreply.github.com>
2023-02-26 22:07:47 -0800

    Fixed bugs with previous commit
    Home

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour {
 
-    [SerializeField] private InputHandler inputHandler;
+  [SerializeField] private InputHandler inputHandler;
 
-    [SerializeField] private GameObject selectedGraphics;
+  [SerializeField] private GameObject selectedGraphics;
 
-    private bool inRange;
+  private bool inRange;
 
-    private void Update() {
-        if (inputHandler.GetInteractKey() == 1 && inRange) {
-            InitiatePickUp();
-        }
+  private void Update() {
+    if (inputHandler.GetInteractKey() == 1 && inRange) {
+      InitiatePickUp();
     }
+  }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Enter");
-        if (other.TryGetComponent<Player>(out Player Component)) {
-            inRange = true;
-            ShowGraphics();
-        }
+  private void OnTriggerEnter2D(Collider2D other) {
+    Debug.Log("Enter");
+    if (other.TryGetComponent<Player>(out Player Component)) {
+      inRange = true;
+      ShowGraphics();
     }
+  }
 
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.TryGetComponent<Player>(out Player Component)) {
-            inRange = false;
-            HideGraphics();
-        }
+  private void OnTriggerExit2D(Collider2D other) {
+    if (other.TryGetComponent<Player>(out Player Component)) {
+      inRange = false;
+      HideGraphics();
     }
+  }
 
-    private void ShowGraphics() {
-        selectedGraphics.SetActive(true);
-    }
+  private void ShowGraphics() {
+    selectedGraphics.SetActive(true);
+  }
 
-    private void HideGraphics() {
-        selectedGraphics.SetActive(false);
-    }
+  private void HideGraphics() {
+    selectedGraphics.SetActive(false);
+  }
 
-    private void InitiatePickUp() {
-
-    }
+  private void InitiatePickUp() {
+    Debug.Log("Collect");
+    gameObject.SetActive(false);
+  }
 
 }
